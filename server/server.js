f75af110c8d4556134d2c39b47e85c014d315d57
Anthony Sim <anthonyysim@gmail.com>
2021-03-19 08:55:17 -0700

    fixed reviewsApp to render on proxy

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const cors = require('cors');
 const axios = require('axios');
+
 const dotenv = require('dotenv');
 dotenv.config({ path: 'config/config.env' });
 
@@ -16,34 +17,39 @@ app.use(express.static(path.join(__dirname, '../public')));
 // middleware for data sent to database
 app.use(express.json());
 
+
 // GET Request for proxy
 app.get('/:id', (req, res) => {
   res.sendFile(path.resolve('public/index.html'));
 });
 
+
 // GET Request from "IMAGE GALLERY" Server Port
 app.get('/images/:id', async (req, res) => {
   const { data } = await axios.get(`http://localhost:4012/images/${req.params.id}`)
+  console.log(data)
   res.send(data);
 })
 
-// GET Request from "REVIEWS" Server Port
+// // GET Request from "REVIEWS" Server Port
 app.get('/reviews/:id', async (req, res) => {
-  const { data } = await axios.get(`http://localhost:4052/reviews/${req.params.id}`)
-  res.send(data)
-})
-
-// GET Request from "MORE LIKE THIS" Server Port
-app.get('/morelikethis/:id', async (req, res) => {
-  await axios.get(`http://localhost:4022/morelikethis/${req.params.id}`)
-    .then(response => {
-      res.send(response.data);
-    })
-    .catch(err => {
-      console.log('Error with GET request to server', err);
-      res.status(404).end();
-    });
+  const { data } = await axios.get(`http://localhost:4052/reviews/${req.params.id}`);
+  console.log(data)
+  res.send(data);
 });
+
+
+// // GET Request from "MORE LIKE THIS" Server Port
+// app.get('/morelikethis/:id', async (req, res) => {
+//   await axios.get(`http://localhost:4022/morelikethis/${req.params.id}`)
+//     .then(response => {
+//       res.send(response.data);
+//     })
+//     .catch(err => {
+//       console.log('Error with GET request to server', err);
+//       res.status(404).end();
+//     });
+// });
 
 // server connection
 const PORT = process.env.PORT || 3000;
