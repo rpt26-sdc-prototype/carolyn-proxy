53aee1d542321be0ddb65870a59139605e59240f
Anthony Sim <anthonyysim@gmail.com>
2021-03-23 09:30:19 -0700

    removed nav

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 
 // components
 import Header from './components/Header.jsx';
-import NavBar from './components/NavBar.jsx';
+// import NavBar from './components/NavBar.jsx';
 import Footer from './components/Footer.jsx';
 
 // components rendered to DOM
 ReactDOM.render(<Header />, document.getElementById('header'));
-ReactDOM.render(<NavBar />, document.getElementById('nav'));
+// ReactDOM.render(<NavBar />, document.getElementById('nav'));
 ReactDOM.render(<Footer />, document.getElementById('footer'));
